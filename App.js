92a4efa4be0cf22c308082b4f970b976c5a4b0ee
Justin Fischer <jfischer@adadapted.com>
2023-09-29 18:18:24 -0400

    added proof of concept for hamburger button'

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, Alert} from 'react-native';
+import React, { Component } from 'react';
+// import { Header } from 'react-native-elements';
+// import { Left, Right, Icon } from 'native-base';
+
+
 
 export default function App() {
   return (
+
+    // <TouchableHighlight onPress={() => this.moveToAddNewCustomer()}>
+    //     <Image style={styles.imagestyle} source={require('./ic_action_name.png')} />
+    // </TouchableHighlight>
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableHighlight onPress={() => Alert.alert("Hello Ryan!")}>
+        <Image
+          source={require('./images/hamburger_icon.png')}
+          style={{width: 40, height: 40, marginTop: 60, marginLeft: 30}}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    fontSize: 64,
   },
 });
