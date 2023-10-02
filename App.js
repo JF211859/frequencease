288b32ca66206f3567a56ebc15a659f68a5ce5b4
awchen61 <awchen61@gmail.com>
2023-10-02 17:39:21 -0400

    Add hamburger menu and blank views

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import FrequencyAdjusterPage from './Components/frequencyAdjusterPage.js'
+import "react-native-gesture-handler";
+import FrequencyAdjuster from "./Components/FrequencyAdjuster";
+import FrequencyTester from "./Components/FrequencyTester";
+import Settings from "./Components/Settings";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-
-    return (
-        <FrequencyAdjusterPage>
-        </FrequencyAdjusterPage>
-    );
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName={"Home"}
+        screenOptions={{
+          headerStyle: { backgroundColor: "transparent" },
+          headerTitleStyle: {
+            fontSize: 25,
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="Home"
+          component={FrequencyAdjuster}
+          options={{ headerTitle: "Frequency Adjuster" }}
+        />
+        <Drawer.Screen name="Frequency Tester" component={FrequencyTester} />
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
