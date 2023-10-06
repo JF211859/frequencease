7c227313423b0c3205511705a54919b8e15865ce
sjxu <sjxu@umich.edu>
2023-10-06 12:59:19 -0400

    created view for frequency tester

--- Components/FrequencyTester.js
+++ Components/FrequencyTester.js
@@ -1,10 +1,84 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, Dimensions, Button} from 'react-native';
+// import ProgressBar from "react-native-progress/Bar";
+// import SemiCircleProgressBar from "react-progressbar-semicircle";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+
 
 // This is the main view for the Frequency Tester
 // First time users should be directed to this view first
+// <SemiCircleProgressBar percentage={33} showPercentValue />
+// <Progress.Bar progress={0.1} width={200} />
 export default function FrequencyTester() {
-  return <View></View>;
+  const circlePosition = (windowWidth / 2) - 115;
+  const playPausePosition = (windowWidth / 2) - 95;
+  const replayPosition = 90;
+  const thumbsUpPostion = (windowWidth / 2) - 100;
+  const thumbsDownPosition = 90
+
+  const audio = () => {
+    Alert.alert("implement audio playback and button switching (play/pause) for alpha");
+  }
+  const testerPhasePage = () => {
+    Alert.alert("navigate to phase page here after merging");
+  }
+  const testerTutorialPage = () => {
+    Alert.alert("tutorial page to be implemented");
+  }
+
+  return (
+    <View>
+      <View style={{ width: 230, height: 230, borderRadius: 115, borderColor: "grey", borderWidth: 5, marginLeft: circlePosition, marginTop: 50 }}>
+        <Text style={{ fontSize: 52, marginTop: 80, textAlign: "center", color: "black" }}>
+          20 Hz
+        </Text>
+      </View>
+
+      <View style={{flexDirection: "row"}}>
+        <TouchableOpacity onPress={audio}>
+          <Image
+            source={require('../images/playbutton.png')}
+            style={{width: 55, height: 55, marginTop: 50, marginLeft: playPausePosition}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={audio}>
+          <Image
+            source={require('../images/replaybutton.png')}
+            style={{width: 60, height: 60, marginTop: 50, marginLeft: replayPosition}}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <Text style={{ fontSize: 32, marginTop: 100, textAlign: "center" }}>
+          Can you hear this sound?
+      </Text>
+
+      <View style={{flexDirection: "row"}}>
+        <TouchableOpacity onPress={testerPhasePage}>
+          <Image
+            source={require('../images/thumbsup.png')}
+            style={{width: 55, height: 55, marginTop: 40, marginLeft: thumbsUpPostion}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={testerPhasePage}>
+          <Image
+            source={require('../images/thumbsdown.png')}
+            style={{width: 55, height: 55, marginTop: 40, marginLeft: thumbsDownPosition}}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity onPress={testerTutorialPage}>
+        <Image
+          source={require('../images/tutorial.png')}
+          style={{ width: 50, height: 50, marginLeft: windowWidth - 70, marginTop: 115 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
