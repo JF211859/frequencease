1ebe55abfe4f18c231a2043d33b65e33bc6ba141
riwang <riwang@umich.edu>
2023-11-14 20:55:59 -0500

    Changes

--- Components/FrequencyAdjuster/FrequencyAdjuster.js
+++ Components/FrequencyAdjuster/FrequencyAdjuster.js
@@ -29,12 +29,12 @@ export default function FrequencyAdjuster() {
   const [isPlayerModalVisible, setPlayerModalVisible] = React.useState(false);
   
   const recordModalNext = () => {
-    setRecordModalVisible(false);
     setImportModalVisible(true);
+    setRecordModalVisible(false);
   };
   const importModalNext = () => {
-    setImportModalVisible(false);
     setPlayerModalVisible(true);
+    setImportModalVisible(false);
   };
   const closeAll = () => {
     setRecordModalVisible(false);
@@ -74,130 +74,155 @@ export default function FrequencyAdjuster() {
         backdropOpacity={0.8}
         >
         <View
-            style={[
-            styles.center,
+          style={[
+          styles.center,
             {
-                width: 300,
-                height: 300,
-                backgroundColor: "white",
+              width: 300,
+              height: 300,
+              backgroundColor: "white",
+              borderRadius: 30,
+              padding: 20,
+            },
+          ]}
+        >
+          <Text style={[styles.h3, { paddingBottom: 20, marginTop: 20 }]}>
+            Record
+          </Text>
+          <Text style={styles.body}>
+            Tap the Record button to start recording.
+            When you are done recording, tap the button again to stop recording.
+            The audio you recorded will be adjusted in the app!
+          </Text>
+
+          <View style={[styles.row, { justifyContent: "space-around" }]}>
+            <TouchableOpacity
+            style={[
+            styles.button,
+            {
                 borderRadius: 30,
-                padding: 20,
+                backgroundColor: COLORS.RED,
+                marginRight: 10
             },
             ]}
-        >
-            <Text style={[styles.h3, { paddingBottom: 20, marginTop: 20 }]}>
-                Record
-            </Text>
-            <Text style={[styles.body, { marginTop: 20 }]}>
-                Tap the Record button to start recording.
-                When you are done recording, tap the button again to stop recording.
-                The audio you recorded will be adjusted in the app!
-            </Text>
-
-            <View style={[styles.row, { justifyContent: "space-around" }]}>
-                <TouchableOpacity
-                  style={[
-                  styles.button,
-                  {
-                      borderRadius: 30,
-                      backgroundColor: COLORS.RED,
-                      paddingRight: 5
-                  },
-                  ]}
-                  onPress={() => closeAll()}
-                >
-                    <Text style={styles.h3}>Close</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[
-                    styles.button,
-                    {
-                        borderRadius: 30,
-                        backgroundColor: COLORS.LIGHT_BLUE,
-                        paddingLeft: 5
-                    },
-                    ]}
-                    onPress={() => recordModalNext()}
-                >
-                    <Text style={styles.h3}>Next</Text>
-                </TouchableOpacity>
-            </View>
+            onPress={() => closeAll()}
+            >
+              <Text style={styles.h3}>Close</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+              styles.button,
+              {
+                  borderRadius: 30,
+                  backgroundColor: COLORS.LIGHT_BLUE,
+                  marginLeft: 10
+              },
+              ]}
+              onPress={() => recordModalNext()}
+            >
+              <Text style={styles.h3}>Next</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        </Modal>
-
-        <Modal
+      </Modal>
+      <Modal
         isVisible={isImportModalVisible}
         style={styles.center}
         backdropOpacity={0.8}
         >
-        <Text style={[styles.h3, { paddingBottom: 20, marginTop: 20 }]}>
-                Import File
-            </Text>
-            <Text style={[styles.body]}>
-                Tap the Import File to open your device's file selector.
-                Choose the file you want to listen to.
-                The audio you selected will be adjusted in the app!
-            </Text>
+        <View
+          style={[
+          styles.center,
+            {
+              width: 300,
+              height: 300,
+              backgroundColor: "white",
+              borderRadius: 30,
+              padding: 20,
+            },
+          ]}
+        >
+          <Text style={[styles.h3, { paddingBottom: 20, marginTop: 20 }]}>
+            Import
+          </Text>
+          <Text style={styles.body}>
+            Tap the Import File to open your device's file selector.
+            Choose the file you want to listen to.
+            The audio you selected will be adjusted in the app!
+          </Text>
 
-            <View style={[styles.row, { justifyContent: "space-around" }]}>
+          <View style={[styles.row, { justifyContent: "space-around" }]}>
             <TouchableOpacity
-                  style={[
-                  styles.button,
-                  {
-                      borderRadius: 30,
-                      backgroundColor: COLORS.RED,
-                      paddingRight: 5
-                  },
-                  ]}
-                  onPress={() => closeAll()}
-                >
-                    <Text style={styles.h3}>Close</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[
-                    styles.button,
-                    {
-                        borderRadius: 30,
-                        backgroundColor: COLORS.LIGHT_BLUE,
-                        paddingLeft: 5
-                    },
-                    ]}
-                    onPress={() => importModalNext()}
-                >
-                    <Text style={styles.h3}>Next</Text>
-                </TouchableOpacity>
-            </View>
-        </Modal>
+            style={[
+            styles.button,
+            {
+                borderRadius: 30,
+                backgroundColor: COLORS.RED,
+                marginRight: 10
+            },
+            ]}
+            onPress={() => closeAll()}
+            >
+              <Text style={styles.h3}>Close</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+              styles.button,
+              {
+                  borderRadius: 30,
+                  backgroundColor: COLORS.LIGHT_BLUE,
+                  marginLeft: 10
+              },
+              ]}
+              onPress={() => importModalNext()}
+            >
+              <Text style={styles.h3}>Next</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
-        <Modal
-        isVisible={isPlayerModalVisible}
+      <Modal
+        isVisible={isRecordModalVisible}
         style={styles.center}
         backdropOpacity={0.8}
         >
-            <Text style={[styles.h3, { paddingBottom: 20, marginTop: 20 }]}>
-                Listening to files
-            </Text>
-            <Text style={[styles.body, { marginTop: 20 }]}>
-                Use our built-in audio player to listen to adjusted audio!
-                Use the Play, Pause, and Stop buttons to control the audio playback.
-                Use the Replay button to start the audio from the beginning.
-            </Text>
+        <View
+          style={[
+          styles.center,
+            {
+              width: 300,
+              height: 300,
+              backgroundColor: "white",
+              borderRadius: 30,
+              padding: 20,
+            },
+          ]}
+        >
+          <Text style={[styles.h3, { paddingBottom: 20, marginTop: 20 }]}>
+            Using the Player
+          </Text>
+          <Text style={styles.body}>
+            Use our built-in audio player to listen to adjusted audio!
+            Use the Play, Pause, and Stop buttons to control the audio playback.
+            Use the Replay button to start the audio from the beginning.
+          </Text>
 
-            <View style={[styles.row, { justifyContent: "space-between" }]}>
-                <TouchableOpacity
-                    style={[
-                    styles.button,
-                    {
-                        borderRadius: 30,
-                        backgroundColor: COLORS.RED,
-                    },
-                    ]}
-                    onPress={() => closeAll()}
-                >
-                    <Text style={styles.h3}>Close</Text>
-                </TouchableOpacity>
-            </View>
-        </Modal>
+          <View style={[styles.row, { justifyContent: "space-around" }]}>
+            <TouchableOpacity
+            style={[
+            styles.button,
+            {
+                borderRadius: 30,
+                backgroundColor: COLORS.RED,
+            },
+            ]}
+            onPress={() => closeAll()}
+            >
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
       <Text style={[styles.h1, styles.marginTop, styles.center]}>
         Audible Frequency
       </Text>
